33c110defe2022ce97a1732933e732b2e67dd82c
Sky Winston <skywinston@gmail.com>
2015-11-17 14:03:14 -0700

    changes to gitignore to ignore .env file

--- utils/authenticate.js
+++ utils/authenticate.js
@@ -5,7 +5,7 @@ function authenticate(req, res, next){
     res.status(400).end("Must provide username or password");
   }
 
-  // ++ Look up the user in the DB right here and see if the UN and PW matches ++
+  
   if(body.username !== user.username || body.password !== user.password){
     // if it doesn't...
     res.status(401).end("Username or password incorrect");
