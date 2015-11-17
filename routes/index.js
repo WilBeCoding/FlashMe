5287ded430dfdb46c3f77e77aec089d5c6c5f018
Sky Winston <skywinston@gmail.com>
2015-11-17 13:09:39 -0700

    setting up serverside route for login, update bin www to user server instead of app, deleted the users.js route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var authenticate = require('../utils/authenticate');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', authenticate, function(req, res, next){
+  // Passed authentication!
+  res.send(user);
+})
 
 module.exports = router;
