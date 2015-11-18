423b0170beb3ee6914f4f415da2eea04c5f4e264
Kelly Byrne <kellyannbyrne@utexas.edu>
2015-11-18 13:44:06 -0700

    wildcard route

--- public/controllers/SubjectController.js
+++ public/controllers/SubjectController.js
@@ -1,5 +1,8 @@
 app.controller('SubjectController', ['$scope', '$location', '$http', '$rootScope', function($scope, $location, $http, $rootScope){
 	
+	$http.get('/createcard/:id').then(function(response){
+		console.log("RESPONSE FROM CONTROLLER:", response)
+	})
 
 	$scope.addCard = function(){
 		console.log("user:", $rootScope.user)
--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,11 @@ var app = express();
 
 app.use(expressJwt({ secret: "secret" }).unless({ path: ['/login'] }));
 
+router.get('*', function(req, res, next) {
+  res.sendFile('index.html', { 
+    root: __dirname + '/../public/' 
+  })
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -50,24 +55,38 @@ router.post('/register', function(req, res, next){
   });
 });
 
+router.get('/createcard', function(req, res, next){
+  pg.connect(process.env.DB_URI, function(err, client, done){
+    done();
+    console.log("USER for subject query: ", req.body.user)
+    client.query('SELECT * FROM users WHERE email = $1', [req.body.user], function(err, result){
+      done();
+      client.query('SELECT * FROM subjects WHERE user_id = $1', [result.rows[0].id], function(err, result){
+        done();
+        res.json(result);
+      })
+    })
+  })
+})
 
 router.post('/createcard', function(req, res, next) {
   // test if new subject or existing for different routes, currently only setup with new subject and hardcoded user
   pg.connect(process.env.DB_URI, function(err, client, done) {
-    console.log("Body: ", req.body);
-    client.query('SELECT * FROM subjects WHERE name=$1', [req.body.newSubject], function(err, result){
-      console.log("Search for subject result: ", result)
-       client.query('INSERT INTO subjects VALUES (default, (SELECT id FROM users WHERE email=$1), $2)', [ req.body.user, req.body.newSubject], function(err, result) {
-         client.query('INSERT INTO cards VALUES (default, $2, $3, (SELECT id FROM subjects WHERE name=$1), 1)', [req.body.newSubject, req.body.question, req.body.answer], function(err, result){
-          console.log('error: ', err)
-          console.log('result', result);
-          done();
-          res.json(result);
-         })
+    client.query('SELECT * FROM users WHERE email=$1', [req.body.user], function(err, result){
+      client.query('SELECT * FROM subjects WHERE name=$1 AND user_id=$2', [req.body.newSubject, result.rows[0].id], function(err, result){
+         client.query('INSERT INTO subjects VALUES (default, (SELECT id FROM users WHERE email=$1), $2)', [ req.body.user, req.body.newSubject], function(err, result) {
+           client.query('INSERT INTO cards VALUES (default, $2, $3, (SELECT id FROM subjects WHERE name=$1), 1)', [req.body.newSubject, req.body.question, req.body.answer], function(err, result){
+            console.log('error: ', err)
+            console.log('result', result);
+            done();
+            res.json(result);
+           })
 
-      });
+        });
 
+      })
     })
+    
 
   });
 });
