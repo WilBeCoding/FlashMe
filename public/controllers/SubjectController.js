8292e6a8747b7e0d164c6653728855b00fc3d0d9
Wilson Rondini <wilbecoding@gmail.com>
2015-11-18 15:45:04 -0700

    User now sees the Question side of the flash card first and upon submiting is shown the Answer side

--- public/controllers/SubjectController.js
+++ public/controllers/SubjectController.js
@@ -1,13 +1,18 @@
 app.controller('SubjectController', ['$scope', '$location', '$http', '$rootScope', function($scope, $location, $http, $rootScope){
-	
+	$scope.visible = false;
+  $scope.showAnswer = function(){
+    console.log('ShowAnswer is running');
+    $scope.visible = (!$scope.visible)? true: false;
+  };
 
 	$scope.addCard = function(){
 		//http.post to cards table
 		$http.post("/createcard", $scope.newCard);
 		console.log($scope.newCard)
-
 		$scope.newCard = {};
 	}
+
+
 	
 }])
 
