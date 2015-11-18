2ae358aba32cdfd133d149efd8b36c0b97733a42
Sky Winston <skywinston@gmail.com>
2015-11-18 11:36:49 -0700

    logout is now wired up and deletes the auth-token from local storage

--- public/controllers/LoginController.js
+++ public/controllers/LoginController.js
@@ -1,34 +1,10 @@
-app.factory('UserFactory', function UserFactory($http, AuthTokenFactory){
-  'use strict';
-  return {
-    login: login,
-    logout: logout
-  };
-
-  function login (email, password){
-    return $http.post('/login', {
-      email: email,
-      password: password
-    }).then(function success(response){
-      AuthTokenFactory.setToken(response.data.token);
-      return response;
-    });
-  }
-
-  function logout(){
-    AuthTokenFactory.setToken();  // Sets token to nothing removing it from localStorage.
-  }
-});
-
 app.controller('LoginController', ['$scope', '$http', 'UserFactory', function LoginController($scope, $http, UserFactory){
 
   $scope.userLogin = logUserIn;
 
   function logUserIn(username, password){
     UserFactory.login(username, password).then(function success(response){
-      console.log(response.data);
       $scope.user = response.data.user;
-      localStorage.setItem('token', response.data.token);
     }, handleError);
   }
 
