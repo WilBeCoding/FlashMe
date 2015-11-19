7681bcd94c0c717735c9897704feb6a676bca49b
Sky Winston <skywinston@gmail.com>
2015-11-19 13:28:32 -0700

    fix to brackets on SubjectController

--- public/controllers/DashController.js
+++ public/controllers/DashController.js
@@ -3,32 +3,31 @@ app.controller('DashController', ['$scope', '$location', '$http', '$rootScope', 
   	var user = UserFactory.readUser();
   	$http.defaults.headers.common.user = user;
 
-	$http({
-		method: 'GET',
-		url: '/newcard'
-	}).then(function success(response){
-		$scope.subjects = response.data;
-		$scope.subjects.forEach(function(each){
-			each.selected = 'false';
-		})
-		console.log("SCOPE.SUBJECTS", $scope.subjects);
-	}, function error(response){
-		console.log(response);
+  	$http({
+  		method: 'GET',
+  		url: '/newcard'
+  	}).then(function success(response){
+  		$scope.subjects = response.data;
+  		$scope.subjects.forEach(function(each){
+  			each.selected = 'false';
+  		})
+  		console.log("SCOPE.SUBJECTS", $scope.subjects);
+  	}, function error(response){
+  		console.log(response);
 
-	}, function error(){
-    $state.go('login');
+  	}, function error(){
+      $state.go('login');
+    });
+
+  	$scope.selectSubjects = function(){
+  		console.log("SCOPE CONTROLLER DATA:", $scope.subjects);
+  		$http({
+  			method: 'POST',
+  			url: '/subjects',
+  			data: {subjects: $scope.subjects}
+  		}).then(function success(response){
+  			console.log("CONTROLLER RESPONSE:", response)
+  		})
+  	}
   });
-	
-	$scope.selectSubjects = function(){
-		console.log("SCOPE CONTROLLER DATA:", $scope.subjects);
-		$http({
-			method: 'POST',
-			url: '/subjects',
-			data: {subjects: $scope.subjects}
-		}).then(function success(response){
-			console.log("CONTROLLER RESPONSE:", response)
-		})
-	}
-
-
-}])
+}]);
