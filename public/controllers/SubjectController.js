246827d99cc59850839e4f02358209c3bae5f6ed
Wilson Rondini <wilbecoding@gmail.com>
2015-11-18 19:37:21 -0700

    Question/Answer createCard flow updated. ng-show question on answer page

--- public/controllers/SubjectController.js
+++ public/controllers/SubjectController.js
@@ -1,6 +1,6 @@
 app.controller('SubjectController', ['$scope', '$location', '$http', '$rootScope', function($scope, $location, $http, $rootScope){
 	$scope.visible = false;
-  $scope.showAnswer = function(){
+  $scope.toggleInputs = function(){
     $scope.visible = (!$scope.visible)? true: false;
   };
 
