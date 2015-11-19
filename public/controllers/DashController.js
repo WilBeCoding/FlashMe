b1dc7b080cfee8ed973ae658c6f4c4e3a79c071c
Sky Winston <skywinston@gmail.com>
2015-11-19 13:05:18 -0700

    check for authenticated user on Dashboard and New Card creeation

--- public/controllers/DashController.js
+++ public/controllers/DashController.js
@@ -1,17 +1,19 @@
-app.controller('DashController', ['$scope', '$location', '$http', '$rootScope', 'UserFactory', function($scope, $location, $http, $rootScope, UserFactory){
-  UserFactory.getUser();
-	var user = UserFactory.readUser();
-	$http.defaults.headers.common.user = user;
+app.controller('DashController', ['$scope', '$location', '$http', '$rootScope', 'UserFactory', '$state', function($scope, $location, $http, $rootScope, UserFactory, $state){
+  UserFactory.getUser().then(function success(){
+  	var user = UserFactory.readUser();
+  	$http.defaults.headers.common.user = user;
 
-	$http({
-		method: 'GET',
-		url: '/newcard'
-	}).then(function success(response){
-		$scope.subjects = response
-		console.log(response);
-	}, function error(response){
-		console.log(response);
-	});
-	
+  	$http({
+  		method: 'GET',
+  		url: '/newcard'
+  	}).then(function success(response){
+  		$scope.subjects = response
+  		console.log(response);
+  	}, function error(response){
+  		console.log(response);
+  	});
+  }, function error(){
+    $state.go('login');
+  });
+
 }])
-
