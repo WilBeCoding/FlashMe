8dae9e21d0cd808e0ed74c5c29f226b09b79f687
Sky Winston <skywinston@gmail.com>
2015-11-20 11:54:18 -0700

    fixing env var for jwt

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var conString = process.env.DB_URI;
 var bcrypt = require('bcrypt');
 var expressJwt = require('express-jwt');
 
-router.use(expressJwt({ secret: process.env.JWT_TOKEN }).unless({ path: ['/login', '/register', '/favicon.ico'] }));
+router.use(expressJwt({ secret: process.env.JWT_SECRET }).unless({ path: ['/login', '/register', '/favicon.ico'] }));
 
 router.post('/login', authenticate, function(req, res, next){
   // Passed authentication!
